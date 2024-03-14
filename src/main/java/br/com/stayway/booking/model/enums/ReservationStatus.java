3aad5a1411239d73de1bf752f50c98938b36db9c
Fernando Martins <emmanuel.martins@outlook.com>
2024-03-13 23:24:43 -0300

    refactor: change enum variant and add description value

--- src/main/java/br/com/stayway/booking/model/enums/ReservationStatus.java
+++ src/main/java/br/com/stayway/booking/model/enums/ReservationStatus.java
@@ -1,7 +1,17 @@
 package br.com.stayway.booking.model.enums;
 
 public enum ReservationStatus {
-    ACTIVE,
-    MAINTENANCE,
-    CANCELLED
+    CONFIRMED("Confirmed"),
+    MAINTENANCE("Maintenance"),
+    CANCELLED("Cancelled");
+
+    private final String status;
+
+    private ReservationStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
